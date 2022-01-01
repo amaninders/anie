87ef81c76c2d542b27e22654ef602368aada4919
Amaninder Singh <amandeolofficial@gmail.com>
2022-01-01 14:22:44 -0500

    update frontend

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -9,7 +9,7 @@ function Services() {
                     <h2 className="featurette-heading">Workshops</h2>
                     <p className="lead">Whiteness, De-escalation, and Fatphobia</p>
                 </div>
-                <div className="col-md-5">
+                <div className="col-md-5" style={{'backgroundImage': 'url(\'blob.svg\')', 'backgroundRepeat':'no-repeat'}}>
                     <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto img-approach" width="500" height="500" src="pablita-911.png" />
                 </div>
             </div>
@@ -18,7 +18,7 @@ function Services() {
                     <h2 className="featurette-heading">Training</h2>
                     <p className="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
                 </div>
-                <div className="col-md-5 order-md-1">
+                <div className="col-md-5 order-md-1" style={{'backgroundImage': 'url(\'blob.svg\')', 'backgroundRepeat':'no-repeat'}}>
                     <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto img-approach" width="500" height="500" src="pablita-remote-education.png" />
                 </div>
             </div>
@@ -27,7 +27,7 @@ function Services() {
                     <h2 className="featurette-heading">Facilitation</h2>
                     <p className="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
                 </div>
-                <div className="col-md-5">
+                <div className="col-md-5" style={{'backgroundImage': 'url(\'blob.svg\')', 'backgroundRepeat':'no-repeat'}}>
                     <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto img-approach" width="500" height="500" src="pablita-workshop.png" />
                 </div>
             </div>
