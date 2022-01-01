66b5e089645508f6c209675a5b6f2295109ff8c6
Amaninder Singh <amandeolofficial@gmail.com>
2022-01-01 18:13:28 -0500

    removed backend

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '44ad63dae2f7e17ede7129825ef39024'),
-    },
-  },
-});
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'f9ebcc71-3e52-4825-8f3d-305668016236'
-};
